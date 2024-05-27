5d9ab5dd7efb2f42a294ca86e09836c4f322e2d0
LetaoZhao <tonyzhao0227@gmail.com>
2024-05-27 15:26:05 +1200

    smooth avoidance test

--- lib/Movement/MoveToFireUntil_WithAvoidence/MoveToFireUntil_WithAvoidence.cpp
+++ lib/Movement/MoveToFireUntil_WithAvoidence/MoveToFireUntil_WithAvoidence.cpp
@@ -17,41 +17,76 @@ bool MoveToFireUntil_WithAvoidence()
     float left_distance_IR = 0.0; //voltage (V)
     float right_distance_IR = 0.0; //voltage (V)
 
+    static float error = 0;
+    static float error_kp;
+    static float kp = 1000;
+
     while(isRunning) //main loop
     {
         //check if there is an object in front of the robot
-        left_distance_IR = analogRead(AXXXXXX) * 0.0049; //5V 10Bit ADC 
-        right_distance_IR = analogRead(AXXXXXX) * 0.0049; //5V 10Bit ADC 
-        if((left_distance_IR < XXXXXXX)||(left_distance_IR < XXXXXXX)||(HC_SR04_range() < 10))
+        left_distance_IR = IR_sensorReadDistance("41_02");
+        right_distance_IR = IR_sensorReadDistance("41_03");
+        if((left_distance_IR < 150)||(left_distance_IR < 150)||(HC_SR04_range() < 10))
         {
             //check if this is fire
             PhotoTransistor_Read();
-            if((lr_right_avg < XXXXXXX)||(lr_left_avg < XXXXXXX))
+            if((lr_right_avg < 0.45)||(lr_left_avg < 0.45))
             {
+                isRunning  = 0;
                 return true; //if yes, function ended
             }
             else 
             {
                 //start avoidence
-                startAvoidance = true; 
+                bool startAvoidance = true; 
                 while(startAvoidence)
                 {
+                    //cw+
+                    //ccw- 
                     int speed_vel = Calculate_Turning_Potential();
                     left_font_motor.writeMicroseconds(1500 + speed_vel);
                     left_rear_motor.writeMicroseconds(1500 + speed_vel);
                     right_rear_motor.writeMicroseconds(1500 +speed_vel);
                     right_font_motor.writeMicroseconds(1500 + speed_vel);
-                    //cw+
-                    //ccw- 
+
+                    if(speed_vel < 100)
+                    {
+                        startAvoidance = false;
+
+                        int time_start = millis();
+                        int time_gap = millis() - time_start;
+                        while (time_gap < 500)
+                        {
+                            left_font_motor.writeMicroseconds(1500 - 500);
+                            left_rear_motor.writeMicroseconds(1500 - 500);
+                            right_rear_motor.writeMicroseconds(1500 + 500);
+                            right_font_motor.writeMicroseconds(1500 + 500);
+
+                            if((left_distance_IR < 150)||(left_distance_IR < 150)||(HC_SR04_range() < 10))
+                            {
+                                stop();
+                                time_gap += 500;
+                            }
+                        }
+                    }
                 }
             }
         }
 
+        //if no object in front, moving to the fire
+        PhotoTransistor_Read();
+        error = lr_right_avg - lr_left_avg;
+        error_kp = error * kp;
+        //left front, left rear, right rear, right front
+        float motor_speeds[4] = {-speed_val+error_kp,-speed_val+error_kp,speed_val+error_kp,speed_val+error_kp};
+        compute_speed(motor_speeds);
+        left_font_motor.writeMicroseconds(1500 + motor_speeds[0]);
+        left_rear_motor.writeMicroseconds(1500 + motor_speeds[1]);
+        right_rear_motor.writeMicroseconds(1500 + motor_speeds[2]);
+        right_font_motor.writeMicroseconds(1500 + motor_speeds[3]);
+        //Serial1.print(">Error: ");
+        //Serial1.println(error_kp);
     }
-
-    
-    if()
-
 }
 
 
